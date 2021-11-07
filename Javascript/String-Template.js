bb728cf61f3f83d55b6f3392045fcd28f84e88d3
excellencior <turjob44@gmail.com>
2021-11-07 10:28:04 +0600

    String Template

---
+++ Javascript/String-Template.js
@@ -0,0 +1,8 @@
+let name1 = "Taylor";
+let name2 = "Johnny";
+let finalName = `${name1} and ${name2} are engaged now`;
+console.log(finalName);
+
+// Various calculations are possible withing the curly brace
+let finalName2 = `${name1} had ${2+5} pet dogs`;
+console.log(finalName2);
